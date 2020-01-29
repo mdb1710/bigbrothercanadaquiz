3dcea9a871743ab3c0a55b2f840cff27afc160e9
mdb1710 <mdb1710@gmail.com>
2020-01-29 11:43:15 -0500

    changed testquestions.js

--- client/src/TestQuestions.js
+++ client/src/TestQuestions.js
@@ -1,170 +1,115 @@
 const testQuestions = [
     {
         id: "1",
-        name: "Danielle Reyes",
-        answers: ["Season 1", "Season 14", "Season 8", "Season 3"],
-        correctAnswer: "Season 3",
+        name: "Andrew Gordon",
+        answers: ["Season 4", "Season 7", "Season 1", "Season 2"],
+        correctAnswer: "Season 2",
         answered: false
 
     },
     {
         id: "2",
-        name: "Will Kirby",
-        answers: ["Season 18", "Season 5", "Season 2", "Season 12"],
-        correctAnswer: "Season 2",
+        name: "Raul Manriquez",
+        answers: ["Season 1", "Season 4", "Season 7", "Season 5"],
+        correctAnswer: "Season 4",
         answered: false
     },
     {
         id: "3",
-        name: "Curtis Kin",
-        answers: ["Season 19", "Season 1", "Season 13", "Season 3"],
-        correctAnswer: "Season 1",
+        name: "Ika Wong",
+        answers: ["Season 2", "Season 6", "Season 7", "Season 3"],
+        correctAnswer: "Season 2",
         answered: false
 
     },
     {
         id: "4",
-        name: "Jen Diturno",
-        answers: ["BBOTT", "Season 6", "Season 8", "Season 17"],
-        correctAnswer: "Season 8",
-        answered: false
-
-    },
-    {
-        id: "5",
-        name: "Kristen Bitting",
-        answers: ["Season 12", "Season 15", "Season 1", "Season 19"],
-        correctAnswer: "Season 12",
-        answered: false
-
-    },
-    {
-        id: "6",
-        name: "Jeremy McGuire",
-        answers: ["Season 2", "Season 9", "Season 15", "Season 20"],
-        correctAnswer: "Season 15",
-        answered: false
-
-    },
-    {
-        id: "7",
-        name: "Justin Giovinco",
-        answers: ["Season 13", "BBOTT", "Season 8", "Season 4"],
-        correctAnswer: "Season 4",
-        answered: false
-
-    },{
-        id: "8",
-        name: "Jason Guy",
-        answers: ["Season 7", "Season 19", "Season 3", "Season 17"],
+        name: "Bobby Hlad",
+        answers: ["Season 1", "Season 6", "Season 3", "Season 5"],
         correctAnswer: "Season 3",
         answered: false
 
     },
     {
-        id: "9",
-        name: "Jason Agolli",
-        answers: ["Season 17", "Season 5", "BBOTT", "Season 18"],
-        correctAnswer: "Season 17",
+        id: "5",
+        name: "Peter Brown",
+        answers: ["Season 4", "Season 7", "Season 1", "Season 3"],
+        correctAnswer: "Season 1",
         answered: false
 
     },
     {
-        id: "10",
-        name: "Megan Lowder",
-        answers: ["Season 1", "Season 11", "Season 8", "Season 19"],
-        correctAnswer: "Season 19",
-        answered: false
-
-    },
-    {
-        id: "11",
-        name: "Sarah Hrejsa",
-        answers: ["Season 5", "Season 16", "Season 10", "Season 6"],
+        id: "6",
+        name: "Veronica Doherty",
+        answers: ["Season 3", "Season 7", "Season 4", "Season 6"],
         correctAnswer: "Season 6",
         answered: false
 
     },
     {
-        id: "12",
-        name: "Lori Valenti",
-        answers: ["Season 1", "Season 14", "Season 5", "Season 3"],
+        id: "7",
+        name: "Andre Gwenaelle",
+        answers: ["Season 5", "Season 1", "Season 7", "Season 2"],
         correctAnswer: "Season 5",
+        answered: false
+
+    },{
+        id: "8",
+        name: "Mark Drelich",
+        answers: ["Season 5", "Season 2", "Season 7", "Season 3"],
+        correctAnswer: "Season 7",
+        answered: false
+
+    },
+    {
+        id: "9",
+        name: "Jordan Parhar",
+        answers: ["Season 1", "Season 3", "Season 5", "Season 6"],
+        correctAnswer: "Season 3",
+        answered: false
+
+    },
+    {
+        id: "10",
+        name: "Emerald Brady",
+        answers: ["Season 7", "Season 4", "Season 1", "Season 3"],
+        correctAnswer: "Season 1",
+        answered: false
+
+    },
+    {
+        id: "11",
+        name: "Anthony Douglas",
+        answers: ["Season 5", "Season 7", "Season 1", "Season 6"],
+        correctAnswer: "Season 7",
+        answered: false
+
+    },
+    {
+        id: "12",
+        name: "Tim Dormer",
+        answers: ["Season 4", "Season 1", "Season 2", "Season 3"],
+        correctAnswer: "Season 4",
         answered: false
 
     },
     {
         id: "13",
-        name: "Chima Simone",
-        answers: ["Season 12", "Season 1", "Season 9", "Season 11"],
-        correctAnswer: "Season 11",
+        name: "Jon Pardy",
+        answers: ["Season 3", "Season 6", "Season 2", "Season 1"],
+        correctAnswer: "Season 2",
         answered: false
 
     },
     {
         id: "14",
-        name: "Matthew Hoffman",
-        answers: ["Season 19", "Season 3", "Season 12", "Season 10"],
-        correctAnswer: "Season 12",
+        name: "Kaela Grant",
+        answers: ["Season 1", "Season 3", "Season 2", "Season 6"],
+        correctAnswer: "Season 6",
         answered: false
 
     },
-    {
-        id: "15",
-        name: "Bryan Ollie",
-        answers: ["Season 20", "Season 8", "Season 16", "Season 10"],
-        correctAnswer: "Season 10",
-        answered: false
-
-    },
-    {
-        id: "16",
-        name: "Kara Monaco",
-        answers: ["Season 18", "Season 16", "Season 4", "BBOTT"],
-        correctAnswer: "Season 16",
-        answered: false
-
-    },
-    {
-        id: "17",
-        name: "Dan Gheesling",
-        answers: ["Season 10", "Season 14", "Season 6", "Season 9"],
-        correctAnswer: "Season 10",
-        answered: false
-
-    },
-    {
-        id: "18",
-        name: "Michelle Meyer",
-        answers: ["Season 10", "Season 19", "Season 8", "Season 18"],
-        correctAnswer: "Season 18",
-        answered: false
-
-    },
-    {
-        id: "19",
-        name: "Shane Chapman",
-        answers: ["Season 1", "Season 19", 'BBOTT', "Season 6"],
-        correctAnswer: "BBOTT",
-        answered: false
-
-    },
-    {
-        id: "20",
-        name: "Michael Dutz",
-        answers: ["Season 2", "Season 5", "Season 8", "Season 6"],
-        correctAnswer: "Season 8",
-        answered: false
-
-    },
-    {
-        id: "21",
-        name: "Brett Robinson",
-        answers: ["Season 19", "Season 13", "BBOTT", "Season 20"],
-        correctAnswer: "Season 20",
-        answered: false
-
-    },
+    
 ]
 
 export default testQuestions;
